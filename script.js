45d642aab90b2a9e0cb021ba78b5b69b73670459
aattritanath <nathaattrita@gmail.com>
2026-02-12 20:41:31 +0530

    Add all project files including valentine-s-day folder

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded',() => {
+    const animation = lottie.loadAnimation({
+            container: document.getElementById('animation'),
+            path: 'assets/front_animation.json',
+            renderer: 'svg',
+            loop: true,
+            autoplay: true
+        });
+});
+
+    
